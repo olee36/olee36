5afe4db5344eb562cb710b7757b6b2f1570b18fa
olee36 <60064645+olee36@users.noreply.github.com>
2020-01-19 13:25:59 +0100

    Website User System

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'asjh23*HN**D');
+define('DB_NAME', 'login');
+ 
+/* Attempt to connect to MySQL Database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+// Connect to Database
+$con = mysqli_connect("localhost","root","asjh23*HN**D","login");
+    if (mysqli_connect_errno()){
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ die();
+ }
+ 
+ ?>
